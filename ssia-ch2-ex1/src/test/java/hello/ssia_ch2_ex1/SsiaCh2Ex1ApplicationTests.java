f2f4939ca93b126c9ae7d95df1e5f2b620bab22f
송광호 <skh990427@naver.com>
2025-03-02 19:33:55 +0900

    create :: 예제 2-1 프로젝트 생성

---
+++ ssia-ch2-ex1/src/test/java/hello/ssia_ch2_ex1/SsiaCh2Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.ssia_ch2_ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh2Ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
