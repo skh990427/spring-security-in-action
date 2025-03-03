0712c62c99a73393c5ec8a45a619702923d054ac
송광호 <skh990427@naver.com>
2025-03-03 19:32:19 +0900

    feat :: 공부용 HTTPS 키파일 생성, 스프링 설정

---
+++ ssia-ch2-ex1/src/main/java/hello/ssia_ch2_ex1/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package hello.ssia_ch2_ex1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello!";
+    }
+}
